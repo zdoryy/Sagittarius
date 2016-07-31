268cc40c4aa0fb814b0c6d1f2eb48c4bb255f506
elanser <elanserr@gmail.com>
2016-07-31 21:00:42 +0500

    commit LTimerInterval edit

--- mobile/src/main/java/ru/rivendel/sagittarius/LTimerInterval.java
+++ mobile/src/main/java/ru/rivendel/sagittarius/LTimerInterval.java
@@ -4,7 +4,6 @@ package ru.rivendel.sagittarius;
  * Created by elanse on 31.07.16.
  */
 public class LTimerInterval {
-    private Database pDB;
     public int _id;
     public int _id_program;
     public String title;
