9d4187b1f83341fe78d72c54b6920230607d471b
elanser <elanserr@gmail.com>
2016-08-01 20:11:25 +0500

    onDestroy

--- mobile/src/main/java/ru/rivendel/sagittarius/MainActivity.java
+++ mobile/src/main/java/ru/rivendel/sagittarius/MainActivity.java
@@ -55,4 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Environment.db.close();
+    }
 }
